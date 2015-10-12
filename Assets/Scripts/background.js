aaf4c755e4263b9177aa6df2e061730ae9559f83
Jadon Leininger <jleininger@student.neumont.edu>
2015-10-11 21:52:42 -0600

    Tasks are now sorted by date, but dynamic adding and completing is currently broken.

--- Assets/Scripts/background.js
+++ Assets/Scripts/background.js
@@ -2,8 +2,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('main-window.html', {
         'id':'mainWindow',
         'outerBounds': {
-            'width': 400,
-            'height': 550
+            'width': 480,
+            'height': 640
         }
     });
 });
--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -3,17 +3,28 @@ var MainPage = function() {
         mainContainer: null,
         newTaskContainer: null,
         addBtn: null,
-        taskTable,
-        allTasks: [],
+        tables: {
+            today: null,
+            tomorrow: null,
+            next7Days: null,
+            future: null
+        },
+        allTasks: null,
 
         init: function() {
             this.mainContainer = document.getElementById('mainContainer');
             this.newTaskContainer = document.getElementById('newTaskContainer');
-            this.taskTable = document.getElementById('taskTable');
+            this.tables['today'] = document.getElementById('todayTaskTable');
+            this.tables['tomorrow'] = document.getElementById('tomTaskTable');
+            this.tables['next7Days'] = document.getElementById('weekTaskTable');
+            this.tables['future'] = document.getElementById('futureTaskTable');
             this.addBtn = document.getElementById('addBtn');
             this.addBtn.addEventListener('click', this.openAddWindow);
 
-            this.getAllTasks(this.displayAllTasks);
+            this.getAllTasks(function(tasks) {
+                var sortedTasks = taskViewer.sortTasksByDate(tasks);
+                taskViewer.displayAllTasks(sortedTasks);
+            });
         },
         openAddWindow: function() {
             taskViewer.mainContainer.className = 'hidden';
@@ -24,7 +35,7 @@ var MainPage = function() {
             taskViewer.mainContainer.className = 'visible';
             taskViewer.newTaskContainer.className = 'hidden';
         },
-        saveTasks: function() {
+        saveAllTasks: function() {
             chrome.storage.sync.set({'Tasks': taskViewer.allTasks});
         },
         getAllTasks: function(next) {
@@ -38,6 +49,10 @@ var MainPage = function() {
                 next(taskViewer.allTasks);
             });
         },
+        saveTask: function(task) {
+            var taskIndex = taskViewer.allTasks.push(task);
+            taskViewer.createTaskDisplay(taskViewer.allTasks[taskIndex - 1], taskIndex - 1);
+        },
         completeTask: function(index) {
             var currentClass = taskViewer.taskTable.rows[index + 1].className;
 
@@ -47,15 +62,50 @@ var MainPage = function() {
         deleteTask: function(index) {
             taskViewer.allTasks.splice(index, 1);
             taskViewer.taskTable.deleteRow(index + 1);
-            this.saveTasks();
+            this.saveAllTasks();
+        },
+        sortTasksByDate: function(tasks) {
+            var tasksByDates = {
+                today: [],
+                tomorrow: [],
+                next7Days: [],
+                future: []
+                },
+                today = new Date(),
+                tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1),
+                next7Days = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
+;
+            for(var i = 0; i < tasks.length; i++) {
+                var dueDate = new Date(tasks[i].dueDate),
+                    dueTime = tasks[i].dueTime;
+
+                dueDate.setDate(dueDate.getDate() + 1);
+
+                if(dateCalc.compareDay(today, dueDate) === 0) {
+                    tasksByDates.today.push(tasks[i]);
+                } else if(dateCalc.compareDay(tomorrow, dueDate) === 0) {
+                    tasksByDates.tomorrow.push(tasks[i]);
+                } else if(dateCalc.compareDay(dueDate, next7Days) < 0) {
+                    tasksByDates.next7Days.push(tasks[i]);
+                } else {
+                    tasksByDates.future.push(tasks[i]);
+                }
+            }
+
+            return tasksByDates;
         },
         displayAllTasks: function(tasks) {
-            for(var i = 0; i < tasks.length; i++) {
-                taskViewer.createTaskDisplay(tasks[i], i);
+            for(var taskCategory in tasks) {
+                if(tasks.hasOwnProperty(taskCategory)) {
+                    for(var i = 0; i < tasks[taskCategory].length; i++) {
+                        console.log(tasks[taskCategory]);
+                        taskViewer.createTaskDisplay(tasks[taskCategory][i], i, taskViewer.tables[taskCategory]);
+                    }
+                }
             }
         },
-        createTaskDisplay: function(task, index) {
-            var newRow = taskViewer.taskTable.insertRow(taskViewer.taskTable.length);
+        createTaskDisplay: function(task, index, taskTable) {
+            var newRow = taskTable.insertRow(taskTable.length);
 
             var taskCompleteCell = newRow.insertCell(0);
             var completeCheckBox = document.createElement('input');
@@ -72,7 +122,7 @@ var MainPage = function() {
             taskDescCell.innerHTML = task.desc;
 
             var taskDueDateCell = newRow.insertCell(3);
-            taskDueDateCell.innerHTML = task.dueDate;
+            taskDueDateCell.innerHTML = task.dueDate + '@' + task.dueTime;
 
             var deleteIcon = newRow.insertCell(4);
             var buttonElem = document.createElement('button');
@@ -98,12 +148,16 @@ var MainPage = function() {
             var taskName = document.getElementById('taskName').value,
                 taskDec = document.getElementById('taskDesc').value,
                 taskDueDate = document.getElementById('taskDueDate').value,
+                taskDueTime = document.getElementById('taskDueTime').value,
                 taskReoccurring = document.getElementById('taskReoccurring').checked;
 
-            var task = newTaskWindow.createTask(taskName, taskDec, taskDueDate, taskReoccurring);
-            newTaskWindow.saveTask(task);
+            var task = newTaskWindow.createTask(taskName, taskDec, taskDueDate, taskDueTime, taskReoccurring);
+            taskViewer.saveTask(task);
+
+            taskViewer.closeAddWindow();
+            taskViewer.saveAllTasks();
         },
-        createTask: function(taskName, taskDesc, taskDueDate, reoccurring) {
+        createTask: function(taskName, taskDesc, taskDueDate, taskDueTime, reoccurring) {
             if(!taskName || taskName === '') {
                 window.alert('You must have a name for a new task!');
                 return;
@@ -113,18 +167,13 @@ var MainPage = function() {
                 name: taskName,
                 desc: taskDesc,
                 dueDate: taskDueDate,
+                dueTime: taskDueTime,
                 reoccurring: reoccurring
             };
 
             return newTask;
         },
-        saveTask: function(task) {
-            var taskIndex = taskViewer.allTasks.push(task);
-            taskViewer.closeAddWindow();
-            taskViewer.createTaskDisplay(taskViewer.allTasks[taskIndex - 1], taskIndex - 1);
-            taskViewer.saveTasks();
-        },
-        cancelTask: function() {
+        cancelTaskCreation: function() {
             newTaskWindow.newTaskForm.reset();
             taskViewer.closeAddWindow();
         },
@@ -135,7 +184,31 @@ var MainPage = function() {
 
             //Listeners
             this.createBtn.addEventListener('click', this.addNewTask);
-            this.cancelBtn.addEventListener('click', this.cancelTask);
+            this.cancelBtn.addEventListener('click', this.cancelTaskCreation);
+        }
+    };
+
+    var dateCalc = {
+        convert24to12: function() {
+
+        },
+        compareDay: function(date1, date2) {
+            date1.setHours(0,0,0,0);
+            date2.setHours(0,0,0,0);
+
+            if(date1 > date2) {
+                return 1;
+            } else if(date1 < date2) {
+                return -1;
+            }
+
+            return 0;
+        },
+        compareHour: function() {
+
+        },
+        sortDates: function() {
+
         }
     };
 
